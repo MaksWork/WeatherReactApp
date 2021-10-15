0fbb849c68cc95b8e0f1fae3612f45692bef7ba7
SausageOG <sauageog@gmail.com>
2021-10-15 18:23:35 +0300

    Main info done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,12 +4,15 @@ import styles from "./App.module.css"
 const App = () => {
 	return(
 		<div className={styles.mainContainer}>
-			<div className="mainInfo">
-
+			<div className={styles.mainInfo}>
+				<div className={styles.degrees}>26°</div>
+				<div className={styles.city_time}>
+					<div className={styles.city}>London</div>
+					<div className={styles.time}>10:36 - Tuesday, 22 Oct '19</div>
+				</div>
 			</div>
 			<div className="rightPanelContainer">
 				<div className="locationSearch">
-
 				</div>
 				<div className="details">
 
